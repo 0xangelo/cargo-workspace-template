7f23f692b24d6499888b7076ec4325b63b807711
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2025-01-21 06:31:58 -0300

    feat(hello-world): echo contents of a file

--- crates/hello-world/src/main.rs
+++ crates/hello-world/src/main.rs
@@ -1,5 +1,18 @@
 //! Say hello to the world!
 
-fn main() {
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    path: PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
+    let Cli { path } = Cli::parse();
+    let text = std::fs::read_to_string(path)?;
+    println!("{text}");
+    Ok(())
 }
